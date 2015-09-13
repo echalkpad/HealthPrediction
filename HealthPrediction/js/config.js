a0be882d4d5e53a2b6e8cc4c7faa26136bac3261
Katherine ChengLi <katchengli@gmail.com>
2015-09-13 14:50:02 -0400

    initial commit

---
+++ HealthPrediction/js/config.js
@@ -0,0 +1,10 @@
+define(function(){ 
+	var config = {};
+	config.app = 'HealthPrediction';
+	config.build = 4;
+	config.version = 3.0;
+	config.loginLocation = 'index.html';
+    config.extraScripts=['js/auth.js'];
+	config.dataTransferType = "text";
+	return config;
+});
